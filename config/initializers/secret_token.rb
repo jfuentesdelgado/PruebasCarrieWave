21802c034e73b6f3603e688f5a15b948d39c363c
Jose Fuentes Delgado <jfuentes.jfd@gmail.com>
2014-03-03 19:37:51 +0100

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PruebasCarrieWave::Application.config.secret_key_base = 'b1479ca46e8c04b9ffd69edaeaa9635d519e493126b4ceb1f7680cf84440b3c15ca6e94bba4057147df3f67bdeaa36c9d6a60232d897cf7673affc9d9b7c6773'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PruebasCarrieWave::Application.config.session_store :cookie_store, key: '_PruebasCarrieWave_session'
